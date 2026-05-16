4a0411039791296b52d5882593a21790b03a99f5
Eslam Omar <eslam.O.elkhabery@gmail.com>
2026-05-16 21:20:46 +0300

    feat: implement configuration service to manage environment variables and application settings

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -47,27 +47,3 @@ export const configService = () => {
     }
 };  
 
-//FE configrations
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyB-MpIinkXayArSLCnAN47qnkhVK_lxFuw",
-//   authDomain: "social-media-app-41d4a.firebaseapp.com",
-//   projectId: "social-media-app-41d4a",
-//   storageBucket: "social-media-app-41d4a.firebasestorage.app",
-//   messagingSenderId: "550159446576",
-//   appId: "1:550159446576:web:4f9d8acb8fa84ee87433f5",
-//   measurementId: "G-8G64J06PGW"
-// };
-
-//key -> BA93vVr8dvODRjc3knK0Pzfh6n7aOWqfd-OgcFWjwBW-tIbe0k89l6s7lAP6f1_NudRch-FXwvWme6Uv1jDOO5U
-
-
-//be config
-// var admin = require("firebase-admin");
-
-// var serviceAccount = require("path/to/serviceAccountKey.json");
-
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
